7280f7dc449d5318e60b81e6ba631277dad0a55d
ahmetchinaKA <ahmetchina.ka@gmail.com>
2023-08-31 23:50:33 +0500

    "add an advert to favorites using header favourites button" was deleted

--- tests/addingToFavouritesTests.spec.js
+++ tests/addingToFavouritesTests.spec.js
@@ -22,22 +22,3 @@ test ('add an advert to favorites', async ({ page }) => {
   await expect(product.addToFavouritesHeaderButton.locator('svg')).toHaveAttribute('data-icon', 'favorites-filled');
   await expect(product.addToFavouritesHeaderButton.locator('svg')).toHaveAttribute('name', 'favorites-filled');
 });
-
-
-test ('add an advert to favorites using header favourites button', async ({ page }) => {
-  //Arrange
-  const product = new ProductPage(page);
-
-  //Act
-  await product.addToFavouritesHeaderButton.dispatchEvent('click');
-
-  //Asserts
-  await expect(product.addToFavouritesHeaderButton).toHaveAttribute('data-is-favorite', 'true');
-  await expect(product.addToFavouritesHeaderButton.locator('svg')).toHaveAttribute('data-icon', 'favorites-filled');
-  await expect(product.addToFavouritesHeaderButton.locator('svg')).toHaveAttribute('name', 'favorites-filled');
-
-  await expect(product.addToFavouritesButton).toContainText('В избранном');
-  await expect(product.addToFavouritesButton).toHaveAttribute('data-is-favorite', 'true');
-  await expect(product.addToFavouritesButton.locator('svg')).toHaveAttribute('data-icon', 'favorites-filled');
-  await expect(product.addToFavouritesButton.locator('svg')).toHaveAttribute('name', 'favorites-filled');
-});
